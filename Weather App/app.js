9d0b782a324fa5801e4c39dd73ce31cca3f0d27b
Đình Nam <namndwebdev@gmail.com>
2021-12-25 01:12:59 +0700

    update Day 9

--- Weather App/app.js
+++ Weather App/app.js
@@ -1,46 +1,59 @@
-const input = document.querySelector('.input-search')
+var search = document.querySelector('.search')
+var city = document.querySelector('.city')
+var country = document.querySelector('.country')
+var value = document.querySelector('.value')
+var shortDesc = document.querySelector('.short-desc')
+var visibility = document.querySelector('.visibility span')
+var wind = document.querySelector('.wind span')
+var sun = document.querySelector('.sun span')
+var value = document.querySelector('.value')
+var time = document.querySelector('.time')
+var content = document.querySelector('.content')
+var body = document.querySelector('body')
 
-function changeWeatherUI(weather) {
-	const city = document.querySelector('.name .city')
-	const country = document.querySelector('.name .country')
-	const time = document.querySelector('.time')
-	const temperature = document.querySelector('.temperature .value')
-	const shortDesc = document.querySelector('.short-desc')
 
-	const visibility = document.querySelector('.visibility span')
-	const wind = document.querySelector('.wind span')
-	const cloud = document.querySelector('.cloud span')
+async function changeWeatherUI(capitalSearch){
+    let apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${capitalSearch}&appid=cb8d857e9528680474d1a7a90b239fba
+    `
+    
+    let data = await fetch(apiURL).then(res=> res.json())
+    if(data.cod == 200){
+        content.classList.remove('hide')
+        city.innerText = data.name
+        country.innerText = data.sys.country
+        visibility.innerText = data.visibility + 'm'
+        wind.innerText = data.wind.speed + 'm/s'
+        sun.innerText = data.main.humidity + '%'
+        let temp = Math.round((data.main.temp - 273.15 ))
+        value.innerText = temp
+        shortDesc.innerText = data.weather[0] ? data.weather[0].main : ''
+        time.innerText = new Date().toLocaleString('vi')
+        
+        
+        body.setAttribute('class', 'hot')
+        if(temp <= 25){
+            body.setAttribute('class', 'cool')
+        }
 
-	city.innerHTML = weather.name
-	country.innerHTML = weather.sys.country
-	time.innerHTML = new Date().toLocaleString()
-	shortDesc.innerHTML = weather.weather[0].main
+        if(temp <= 22){
+            body.setAttribute('class', 'warm')
+        }
 
-	const temp = Math.round(weather.main.temp)
-	temperature.innerHTML = temp
-
-	temp >= 18
-		? (document.body.className = 'hot')
-		: (document.body.className = 'cold')
-
-	visibility.innerHTML = weather.visibility + ' (m)'
-	wind.innerHTML = weather.wind.speed + ' (m/s)'
-	cloud.innerHTML = weather.clouds.all + ' (%)'
+        if(temp <= 19){
+            body.setAttribute('class', 'cold')
+        }
+            
+        
+    }else{
+        content.classList.add('hide')
+    }
 }
 
-input.addEventListener('keyup', (e) => {
-	if (e.keyCode === 13) {
-		getWeather(e.target.value)
-	}
+search.addEventListener('keypress', function(e){
+    if(e.code === 'Enter'){
+        let capitalSearch = search.value.trim()
+        changeWeatherUI(capitalSearch)
+    }
 })
 
-async function getWeather(input) {
-	const url = `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=metric&appid=d78fd1588e1b7c0c2813576ba183a667`
-
-	const res = await fetch(url)
-	const weather = await res.json()
-
-	changeWeatherUI(weather)
-}
-
-getWeather('ha noi')
+changeWeatherUI('Ha Noi')
